a53728a81a5881e62c0151ae5e19684952284395
dossmedaziz <dossaziz4@gmail.com>
2023-02-11 19:26:37 +0100

    (update)  : change to the css modules in the layout section

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,23 +1,25 @@
-import {Outlet} from "react-router-dom";
-import "./style.css"
+import { Outlet } from "react-router-dom";
+import styles from "./style.module.css";
 
 function Auth() {
-    return (
-        <div className="container">
-            <div className="quote__side">
-                <div className="quote__container">
-                    <p>Those people who develop the ability to continuously acquire new and better forms of knowledge
-                        that they can apply to their work and to their lives will be the movers and shakers in our
-                        society for the indefinite future</p>
-                    <p>Brian Tracy</p>
-                </div>
-            </div>
-            <div className="form__side">
-                <Outlet/>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.quote__side}>
+        <div className={styles.quote__container}>
+          <p>
+            Those people who develop the ability to continuously acquire new and
+            better forms of knowledge that they can apply to their work and to
+            their lives will be the movers and shakers in our society for the
+            indefinite future
+          </p>
+          <p>Brian Tracy</p>
         </div>
-    );
+      </div>
+      <div className={styles.form__side}>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
-
-export default Auth
+export default Auth;
