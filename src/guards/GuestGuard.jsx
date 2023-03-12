079c0816897f5f44a844f0d516d18780766d83a5
dossmedaziz <dossaziz4@gmail.com>
2023-03-12 10:25:41 +0100

    fix : fix tha auth/guest guards

--- src/guards/GuestGuard.jsx
+++ src/guards/GuestGuard.jsx
@@ -1,14 +1,12 @@
-import { Navigate,nav } from "react-router-dom";
+import { Navigate, nav } from "react-router-dom";
 
-export default function GuestGuard({children}) {
-    // const {isAuthenticated} = useAuth();
-    const isAuthenticated = true
+export default function GuestGuard({ children }) {
+  // const {isAuthenticated} = useAuth();
+  const isAuthenticated = true;
 
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
+  }
 
-    if(isAuthenticated){
-        return <Navigate to="/" />
-    }
-
-
-    return children  
+  return children;
 }
