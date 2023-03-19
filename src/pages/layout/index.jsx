249692b6b4f2c1f9fff5e1bcfa88f2a9f2bb1886
dossmedaziz <dossaziz4@gmail.com>
2023-03-19 16:32:38 +0100

    reformat the code

--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -1,42 +1,39 @@
 import React from "react";
 import style from "./style.module.css";
-import { Outlet } from "react-router-dom";
-import TextInput from "../../components/inputs/textInput";
-import SimpleButton from "../../components/buttons/simpleButton";
 
 export default function Layout() {
-  const [data, setData] = React.useState([]);
+    const [data, setData] = React.useState([]);
 
-  let fakeData = () => {
-    let users = [];
-    for (let i = 0; i < 100; i++) {
-      users.push({ id: i, name: "name", surname: "surname" });
-    }
-    setData(users);
-  };
+    let fakeData = () => {
+        let users = [];
+        for (let i = 0; i < 100; i++) {
+            users.push({id: i, name: "name", surname: "surname"});
+        }
+        setData(users);
+    };
 
-  React.useEffect(() => {
-    fakeData();
-  }, []);
-  return (
-    <>
-      <div className={style.container}>
-        <div className={style.sideBar}>
-          <h1>SideBar</h1>
-        </div>
-        <div className={style.main}>
-          <div className={style.header}></div>
-          <div className={style.content}>
-            {data.map((item, index) => {
-              return (
-                <div style={{ backgroundColor: "red"  , width : '150px' }} key={index}>
-                  {item.name + "-" + index}
+    React.useEffect(() => {
+        fakeData();
+    }, []);
+    return (
+        <>
+            <div className={style.container}>
+                <div className={style.sideBar}>
+                    <h1>SideBar</h1>
                 </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                <div className={style.main}>
+                    <div className={style.header}></div>
+                    <div className={style.content}>
+                        {data.map((item, index) => {
+                            return (
+                                <div style={{backgroundColor: "red", width: '150px'}} key={index}>
+                                    {item.name + "-" + index}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
