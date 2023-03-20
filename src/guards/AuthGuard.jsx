d59f664d4edf19e8a07e23864fd1e0532cbdebd8
dossmedaziz <dossaziz4@gmail.com>
2023-03-20 12:18:21 +0100

    fix the auth/guest guards

--- src/guards/AuthGuard.jsx
+++ src/guards/AuthGuard.jsx
@@ -1,18 +1,12 @@
-import {Navigate} from "react-router-dom";
-import {useAuth} from "../context/useAuth";
-import {useNavigate} from "react-router-dom";
-import {useEffect, useLayoutEffect} from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
 
-export default function AuthGuard({children}) {
-    const {isAuthenticated} = useAuth();
-    console.log("isAuthenticated", isAuthenticated);
-    const navigate = useNavigate();
-    useLayoutEffect(() => {
-            if (!isAuthenticated) {
-                navigate("auth/login");
-            }
-        }
-        , [isAuthenticated]);
+export default function AuthGuard({ children }) {
+  const { isAuthenticated } = useAuth();
+  console.log("isAuthenticated", isAuthenticated);
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
 
-    return children
+  return children;
 }
--- src/guards/GuestGuard.jsx
+++ src/guards/GuestGuard.jsx
@@ -1,18 +1,12 @@
-import {Navigate, nav} from "react-router-dom";
-import {useAuth} from "../context/useAuth";
-import {useNavigate} from "react-router-dom";
-import {useLayoutEffect} from "react";
+import { Navigate, nav } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
 
-export default function GuestGuard({children}) {
-    const {isAuthenticated} = useAuth();
-    console.log("isAuthenticated", isAuthenticated);
-    const navigate = useNavigate();
-    useLayoutEffect(() => {
-            if (isAuthenticated) {
-                navigate("/");
-            }
-        }
-        , [isAuthenticated]);
+export default function GuestGuard({ children }) {
+  const { isAuthenticated } = useAuth();
+  console.log("isAuthenticated", isAuthenticated);
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
+  }
 
-    return children;
+  return children;
 }
