008bddc62482924633720d08a6f6ce354f14fab7
dossmedaziz <dossaziz4@gmail.com>
2023-04-23 20:05:25 +0100

    update : create start button

--- src/components/buttons/simpleButton/index.jsx
+++ src/components/buttons/simpleButton/index.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import styles from "./style.module.css";
-function SimpleButton({ label, onClick  , textColor,bgColor}) {
+function SimpleButton({
+  label,
+  onClick,
+  textColor,
+  bgColor,
+  btnHeight = "64px",
+}) {
   return (
     <div className={styles.button__container}>
       <button
-          onClick={onClick}
-          className={styles.button__shape}
-          style={{ backgroundColor : bgColor , color : textColor}}
+        onClick={onClick}
+        className={styles.button__shape}
+        style={{
+          backgroundColor: bgColor,
+          color: textColor,
+          height: btnHeight,
+        }}
       >
-          {label}
+        {label}
       </button>
     </div>
   );
--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,19 +1,27 @@
-import style from './style.module.css'
-import TextInput from '../../components/inputs/textInput'
-export default function NavBar(){
-    return (
-        <div className={style.navbar__container}>
-         <div className={style.searchbar__container}>
-                 <TextInput 
-                placeholder={'Search'}
-                type={'text'}
-                value={''}
-                onChange={()=>{}}
-                style={{width:'100%' , height:'50px'}}
-                 />
-         </div> 
-         <div className={style.startbtn__container}> start btn</div>
-            <div className={style.avatar__container}>avatar</div>
-        </div>
-    )
+import style from "./style.module.css";
+import TextInput from "../../components/inputs/textInput";
+import SimpleButton from "../buttons/simpleButton";
+export default function NavBar() {
+  return (
+    <div className={style.navbar__container}>
+      <div className={style.searchbar__container}>
+        <TextInput
+          placeholder={"Search"}
+          type={"text"}
+          value={""}
+          onChange={() => {}}
+          style={{ width: "100%", height: "50px" }}
+        />
+      </div>
+      <div className={style.startbtn__container}>
+        <SimpleButton
+          label="Start Quiz"
+          textColor="white"
+          bgColor="#8692A6"
+          btnHeight="54px"
+        />
+      </div>{" "}
+      <div className={style.avatar__container}>avatar</div>
+    </div>
+  );
 }
