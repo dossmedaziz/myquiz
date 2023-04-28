acc2b0d29844c3bb36ec3efb07de8c816080ae06
dossmedaziz <dossaziz4@gmail.com>
2023-04-28 19:31:32 +0100

    fixes : some design and code fixes

--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -2,34 +2,22 @@ import React from "react";
 import style from "./style.module.css";
 import SideBar from "../../components/sideBar";
 import NavBar from "../../components/navBar";
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
+
 export default function Layout() {
-  const [data, setData] = React.useState([]);
-
-  let fakeData = () => {
-    let users = [];
-    for (let i = 0; i < 100; i++) {
-      users.push({ id: i, name: "name", surname: "surname" });
-    }
-    setData(users);
-  };
-
-  React.useEffect(() => {
-    fakeData();
-  }, []);
-  return (
-    <div className={style.container}>
-      <div className={style.sideBar}>
-       <SideBar />
-      </div>
-      <div className={style.main}>
-        <div className={style.header}>
-          <NavBar />
+    return (
+        <div className={style.container}>
+            <div className={style.sideBar}>
+                <SideBar/>
+            </div>
+            <div className={style.main}>
+                <div className={style.header}>
+                    <NavBar/>
+                </div>
+                <div className={style.content}>
+                    <Outlet/>
+                </div>
+            </div>
         </div>
-        <div className={style.content}>
-          <Outlet />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
