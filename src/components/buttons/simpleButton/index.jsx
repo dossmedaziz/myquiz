a6f1d7f8fe92bc8cff992dd6f5ccf715ec77b4f5
dossmedaziz <dossaziz4@gmail.com>
2023-04-30 11:51:11 +0100

    fixes : some design and code fixes

--- src/components/buttons/simpleButton/index.jsx
+++ src/components/buttons/simpleButton/index.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import styles from "./style.module.css";
+
 function SimpleButton({
-  label,
-  onClick,
-  textColor,
-  bgColor,
-  btnHeight = "64px",
-}) {
-  return (
-    <div className={styles.button__container}>
-      <button
-        onClick={onClick}
-        className={styles.button__shape}
-        style={{
-          backgroundColor: bgColor,
-          color: textColor,
-          height: btnHeight,
-        }}
-      >
-        {label}
-      </button>
-    </div>
-  );
+                          label,
+                          onClick,
+                          textColor,
+                          bgColor,
+                          btnHeight = "64px",
+                          style={},
+                      }) {
+    return (
+        <div className={styles.button__container}>
+            <button
+                onClick={onClick}
+                className={styles.button__shape}
+                style={{
+                    backgroundColor: bgColor,
+                    color: textColor,
+                    height: btnHeight,
+                    ...style,
+                }}
+            >
+                {label}
+            </button>
+        </div>
+    );
 }
 
 export default SimpleButton;
